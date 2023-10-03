4f58dd4c043fee6844e1eddf0d69852be26db6bd
mr_sd_jack_003 <barson0habra@gmail.com>
2023-10-03 22:11:59 +0530

    Zoom_Clone_Web

---
+++ Zoom_Clone_Web/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.use('/peerjs', peerServer);
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:room',(req,res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket =>{
+    socket.on('join-room' , (roomId, userId) =>{
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+
+        socket.on('meage', message => {
+            io.to(roomId).emit('createMessage', message);
+        })
+    })
+})
+
+server.listen(process.env.PORT||3030);
