9ae88eff0b435d2fd3290e8eef65c86a2f58f9ed
Akshat Mishra <akshatmishra317@gmail.com>
2023-10-04 12:13:27 +0530

    added Netflix Landing Page

---
+++ Netflix_Landing_Page/index.js
@@ -0,0 +1,6 @@
+//image- https://assets.nflxext.com/ffe/siteui/vlv3/5aecc44d-2a1f-4313-8399-98df20908b64/4d9d5abf-07d8-4dd6-9899-a96b902312ee/IN-en-20221114-popsignuptwoweeks-perspective_alpha_website_medium.jpg
+//tv - https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png
+//phone - https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg
+//phone download - contains pure css content
+// watch everywhere - https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png
+//children - https://occ-0-2164-3211.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABYjXrxZKtrzxQRVQNn2aIByoomnlbXmJ-uBy7du8a5Si3xqIsgerTlwJZG1vMpqer2kvcILy0UJQnjfRUQ5cEr7gQlYqXfxUg7bz.png?r=420
